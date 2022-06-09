bf9835c0118b577478bff1dbaa0e9321c92e1db8
Britney Pellouchoud <59316202+Britney-Pellouchoud@users.noreply.github.com>
2022-06-08 20:10:15 -0700

    Profile finished, next connect github.

--- config/server.js
+++ config/server.js
@@ -1,28 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
 const app = express();
+// Connect Database
+connectDB();
 
-//Connect Database
-connectDB();    
+app.get('/', (req, res) => res.send('API Running'))
 
-//Init Middleware
-const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-
-app.listen(2000)
-
-app.get('/', (req, res) => res.send('API Running'));
-
-// Define Routes
+//Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5000;
 
