e6682feae97e094a3f1e487e2c0ae36df98a1156
Britney Pellouchoud <59316202+Britney-Pellouchoud@users.noreply.github.com>
2022-07-12 08:17:00 -0700

    Created a protected route for dashboard navigation (must be logged in)

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+
 
 import Register from './components/auth/Register';
 
@@ -35,10 +39,10 @@ const App = () => {
      <Routes> 
 
       <Route exact path="/" element = { <Landing/> }/>
-      
-
           <Route exact path="/register" element = {<Register/>} />
           <Route exact path="/login" element = {<Login/>} />
+          <Route path="/dashboard" element = {<PrivateRoute> <Dashboard/> </PrivateRoute>} />
+
       </Routes>
     </Fragment>
   </Router>
