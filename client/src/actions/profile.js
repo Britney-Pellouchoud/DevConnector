770c55ff136fd1306095959f68cd7b4e0de85214
Britney Pellouchoud <59316202+Britney-Pellouchoud@users.noreply.github.com>
2022-07-16 10:07:28 -0700

    Back end data management updated.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,9 +6,11 @@ import { setAlert } from './alert';
 
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     ACCOUNT_DELETED,
+    GET_REPOS,
     CLEAR_PROFILE
 } from './types';
 
@@ -30,6 +32,69 @@ export const getCurrentProfile = () => async dispatch => {
         });
     }
 };
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+      const res = await axios.get('/api/profile');
+
+      dispatch( {
+          type: GET_PROFILES,
+          payload: res.data
+      });
+  } catch (err) {
+      dispatch( {
+          type: PROFILE_ERROR,
+          payload: { msg: err.response.data.msg, status: err.response.status }
+
+
+      });
+  }
+};
+
+// Get profile by id
+export const getProfileById = userId => async dispatch => {
+
+  try {
+      const res = await axios.get(`/api/profile/user/${userId}`);
+
+      dispatch( {
+          type: GET_PROFILE,
+          payload: res.data
+      });
+  } catch (err) {
+      dispatch( {
+          type: PROFILE_ERROR,
+          payload: { msg: err.response.data.msg, status: err.response.status }
+
+
+      });
+  }
+};
+
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+
+  try {
+      const res = await axios.get(`/api/profile/github/${username}`);
+
+      dispatch( {
+          type: GET_REPOS,
+          payload: res.data
+      });
+  } catch (err) {
+      dispatch( {
+          type: PROFILE_ERROR,
+          payload: { msg: err.response.data.msg, status: err.response.status }
+
+
+      });
+  }
+};
+
 
 // Create or update profile
 export const createProfile =
@@ -217,3 +282,4 @@ export const deleteAccount = () => async dispatch => {
   }
 }
 }
+
