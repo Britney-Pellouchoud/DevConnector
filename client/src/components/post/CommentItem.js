54f07c96f28341b380c2e6a11285bf61c9b7c29f
Britney Pellouchoud <59316202+Britney-Pellouchoud@users.noreply.github.com>
2022-07-21 13:53:56 -0700

    Able to delete comment if comment is your own.

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Moment from 'react-moment'
+import { deleteComment } from '../../actions/post'
 
 
 const CommentItem = ({
     postId,
     comment: {_id, text, name, avatar, user, date},
-    auth
+    auth,
+    deleteComment
 }) => 
  (
         <div class="post bg-white p-1 my-1">
@@ -29,6 +31,11 @@ const CommentItem = ({
              <p class="post-date">
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
+            {!auth.loading && user === auth.user._id && (
+                <button onClick={e=> deleteComment(postId, _id)} type="button" className="btn btn-danger">
+                    <i className="fas fa-times"></i>
+                </button>
+            )}
           </div>
         </div>
   )
@@ -37,7 +44,8 @@ const CommentItem = ({
 CommentItem.propTypes = {
     postId: PropTypes.number.isRequired,
     comment: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired,
+    deleteComment: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
@@ -45,4 +53,4 @@ const mapStateToProps = state => ({
 })
 
 
-export default connect(mapStateToProps, { })(CommentItem)
+export default connect(mapStateToProps, { deleteComment })(CommentItem)
